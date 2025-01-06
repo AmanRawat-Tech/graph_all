0f6c960f7b78517ad1a0333d9b69d9c22506537f
aman rawat <amanesyasoft@gmail.com>
2025-01-06 16:43:48 +0530

    changes made

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ st.markdown(
 temp1 = st.selectbox('', ('', 'original dataset', 'modified dataset'))
 if temp1 == 'original dataset':
     if st.button("Go to Original Dataset"):
-        webbrowser.open("https://graph2-hejpjqhrvm6vthevrm4cns.streamlit.app/")
+        st.link_button("Go to Original Dataset","https://graph2-hejpjqhrvm6vthevrm4cns.streamlit.app/")
 elif temp1 == 'modified dataset':
     if st.button("Go to Modified Dataset"):
-        webbrowser.open("https://fcjzyvc2du68da5fhp2rgu.streamlit.app/")
+        st.link_button("Go to Modified Dataset","https://fcjzyvc2du68da5fhp2rgu.streamlit.app/")
 else:
     st.write("Please select a dataset to open a link.")
